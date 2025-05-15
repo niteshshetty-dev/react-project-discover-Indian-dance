1d84a3a4ac98ccd7c740fd2b75b3be7238e9d625
nthshtty <nthsh6shtty@gmail.com>
2025-05-15 15:51:52 +0530

    working on mobile responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <nav className="flex px-6 py-4 justify-between bg-black ">
+      <nav className="flex flex-col sm:flex-row sm:justify-between px-6 py-4 bg-black ">
         <h1 className="text-2xl font-bold text-white">Discover Indian Dance</h1>
         <div className="flex gap-6 ">
           <Link
